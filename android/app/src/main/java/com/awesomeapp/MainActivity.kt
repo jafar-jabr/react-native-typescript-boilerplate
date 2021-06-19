e976630aba75c5ac7a14ceea1fc6986603e0e1b7
Ibrahim Husien <ibrhimhssss@gmail.com>
2021-06-19 12:38:19 +0200

    update

--- android/app/src/main/java/com/awesomeapp/MainActivity.kt
+++ android/app/src/main/java/com/awesomeapp/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.awesomeapp
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import com.facebook.react.ReactActivity
 
 class MainActivity() : ReactActivity() {
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+    }
     override fun getMainComponentName(): String? {
         return "awesomeApp"
     }
